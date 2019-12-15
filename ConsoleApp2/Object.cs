a486110185b427b5bd7df04a02a34d4acf72ee1a
Денис <58759560+KycbFromTomsk@users.noreply.github.com>
2019-12-15 09:10:25 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Object.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    public class Object
+    {
+        public string Address { get; set; }
+        public string FireExtinguishingSystem { get; set; }
+        public string NotificationSystem { get; set; }
+        public string QuantityFireExtinguisher { get; set; }
+        public string EvacuationPlan { get; set; }
+        public string AllInformation
+        {
+            get
+            {
+                return $"{Address}: {FireExtinguishingSystem},{NotificationSystem},{QuantityFireExtinguisher},{EvacuationPlan}";
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Пожалуйста введите адрес объекта");
+                string add = Console.ReadLine();
+
+                switch (add)
+                {
+                    case "Елизаровых 52":
+                        Object Placement1 = new Object();
+                        Placement1.Address = "Елизаровых 52";
+                        Placement1.FireExtinguishingSystem = "СП-есть";
+                        Placement1.NotificationSystem = "СО-нет";
+                        Placement1.EvacuationPlan = "ПЭ-есть";
+                        Placement1.QuantityFireExtinguisher = "огнетушителей-2 шт.";
+                        Console.WriteLine(Placement1.AllInformation);
+                        break;
+
+                    case "Усова 54":
+                        Object Placement2 = new Object();
+                        Placement2.Address = "Усова 54";
+                        Placement2.FireExtinguishingSystem = "СП-есть";
+                        Placement2.NotificationSystem = "СО-есть";
+                        Placement2.EvacuationPlan = "ПЭ-нет";
+                        Placement2.QuantityFireExtinguisher = "огнетушителей-8 шт.";
+                        Console.WriteLine(Placement2.AllInformation);
+                        break;
+
+                    case "Пушкина 43":
+                        Object Placement3 = new Object();
+                        Placement3.Address = "Пушкина 43";
+                        Placement3.FireExtinguishingSystem = "СП-нет";
+                        Placement3.NotificationSystem = "СО-есть";
+                        Placement3.EvacuationPlan = "ПЭ-нет";
+                        Placement3.QuantityFireExtinguisher = "огнетушителей-4 шт";
+                        Console.WriteLine(Placement3.AllInformation);
+                        break;
+
+                    default:
+                        Console.WriteLine("Введен неверный адрес объекта");
+                        break;
+                }
+            }
+        }       
+    }
+}
